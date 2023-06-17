828f48701a233a01cc6501db6d09325d1574d56d
yiwv <y.ishii.wv@gmail.com>
2023-06-18 07:03:34 +0900

    add: sakai-nuxt files

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0 (2023-05-08)
+
+-   The implementation of the Nuxt.js version of the Sakai-Vue project
---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    typescript: false,
+
+    app: {
+        head: {
+            title: 'Sakai Vue',
+            link: [
+                {
+                    id: 'theme-css',
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: '/themes/lara-light-indigo/theme.css'
+                }
+            ]
+        }
+    },
+    build: {
+        transpile: ['primevue']
+    },
+    script: [
+        {
+            strategy: 'lazyOnload',
+            src: 'https://www.googletagmanager.com/gtag/js?id=UA-93461466-1'
+        },
+        {
+            id: 'ga-analytics',
+            strategy: 'lazyOnload',
+            children: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-93461466-1');
+          `
+        }
+    ],
+    css: ['primeicons/primeicons.css', 'primeflex/primeflex.scss', 'primevue/resources/primevue.min.css', '@/assets/styles.scss']
+});
