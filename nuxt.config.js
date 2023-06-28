b59a7f5766d43fc7274e71b2eea21965f7e51e05
yiwv <y.ishii.wv@gmail.com>
2023-06-29 01:10:43 +0900

    mod: nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,13 @@ export default defineNuxtConfig({
 
     app: {
         head: {
-            title: 'Sakai Vue',
+            title: 'Portal Site Template',
             link: [
                 {
                     id: 'theme-css',
                     rel: 'stylesheet',
                     type: 'text/css',
-                    href: '/themes/lara-light-indigo/theme.css'
+                    href: '/themes/lara-light-blue/theme.css'
                 }
             ]
         }
@@ -19,20 +19,6 @@ export default defineNuxtConfig({
         transpile: ['primevue']
     },
     script: [
-        {
-            strategy: 'lazyOnload',
-            src: 'https://www.googletagmanager.com/gtag/js?id=UA-93461466-1'
-        },
-        {
-            id: 'ga-analytics',
-            strategy: 'lazyOnload',
-            children: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-93461466-1');
-          `
-        }
     ],
     css: ['primeicons/primeicons.css', 'primeflex/primeflex.scss', 'primevue/resources/primevue.min.css', '@/assets/styles.scss']
 });
